e2f52c55f1155f85c7b197e59ce2762bf7cd83b5
kwathek <w.kahla16035@pi.tn>
2018-10-22 12:07:12 +0100

    add conf firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bibliotheque';
+  constructor(){
+      // add config firebase for Web Application
+      const config = {
+        apiKey: "AIzaSyD3FrwAvilO6eq3J2s8ZHSKHoACuDF0x7w",
+        authDomain: "http-client-demo-3cb00.firebaseapp.com",
+        databaseURL: "https://http-client-demo-3cb00.firebaseio.com",
+        projectId: "http-client-demo-3cb00",
+        storageBucket: "http-client-demo-3cb00.appspot.com",
+        messagingSenderId: "948278310981"
+      }
+     firebase.initializeApp(config);
+  }
 }
