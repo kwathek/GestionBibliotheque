b35cdf5cd026bad1487fb4fb69af268380cf00c8
kwathek <w.kahla16035@pi.tn>
2018-10-22 11:10:48 +0100

    configuration des URLs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SigninComponent} from "./auth/signin/signin.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+import {BookListComponent} from "./book-list/book-list.component";
+import {BookFormComponent} from "./book-list/book-form/book-form.component";
+import {SingleBookComponent} from "./book-list/single-book/single-book.component";
 
-const routes: Routes = [];
+// Configuration des URLs
+const routes: Routes = [
+  { path: 'auth/signin' , component: SigninComponent},
+  { path: 'auth/signup' , component: SignupComponent},
+  { path: 'books' , component: BookListComponent},
+  { path: 'books/new' , component: BookFormComponent},
+  { path: 'books/view/:id' , component: SingleBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
