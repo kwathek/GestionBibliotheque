5e0cd4da28a1851a94683ef160bc28f0c207885d
kwathek <w.kahla16035@pi.tn>
2018-10-22 14:43:44 +0100

    add general url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,9 @@ const routes: Routes = [
   { path: 'auth/signup' , component: SignupComponent},
   { path: 'books' , canActivate: [AuthGuardService] , component: BookListComponent},
   { path: 'books/new' , canActivate: [AuthGuardService] , component: BookFormComponent},
-  { path: 'books/view/:id' , canActivate: [AuthGuardService] ,  component: SingleBookComponent}
+  { path: 'books/view/:id' , canActivate: [AuthGuardService] ,  component: SingleBookComponent},
+  { path: '' , redirectTo: 'books' , pathMatch: 'full' },
+  { path: '**' , redirectTo: 'books' }
 ];
 
 @NgModule({
