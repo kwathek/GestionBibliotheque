e3744641a4aac8ee8a1fccf826501b8b94a1daa9
kwathek <w.kahla16035@pi.tn>
2018-10-22 16:22:08 +0100

    add Crund Functions to component

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription} from "rxjs";
+import {Book} from "../models/book.model";
+import {BooksService} from "../services/books.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
-
-  constructor() { }
+export class BookListComponent implements OnInit , OnDestroy{
+  books: Book[];
+  booksSubscription: Subscription;
+  constructor( private booksService: BooksService ,
+              private router: Router )  { }
 
   ngOnInit() {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books =books ;
+      }
+    );
+      this.booksService.emitBooks();
+  }
+  onNewBook(){
+    this.router.navigate(['/books','new']);
+  }
+  onDeleteBook(book: Book){
+      this.booksService.removeBook(book);
+  }
+  onViewBook(id: number) {
+      this.router.navigate(['/books' , 'view' , id]);
+  }
+
+  ngOnDestroy(): void {
+      this.booksSubscription.unsubscribe();
   }
 
 }
