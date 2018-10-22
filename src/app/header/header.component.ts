9f2b04bcdac7387d8ee403e4a035bf2e2af38173
kwathek <w.kahla16035@pi.tn>
2018-10-22 12:46:32 +0100

    check connection user

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from "../services/auth.service";
+import * as firbase from 'firebase';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isAuth: boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+      // Check User if connected or not
+      firebase.auth().onAuthStateChanged(
+        (user) => {
+            if(user){
+                this.isAuth = true;
+            }else {
+                this.isAuth = false;
+            }
+        }
+      );
+  }
+
+  onSignOut(){
+      // Call signOut function From authService to disconnect user
+    this.authService.signOutUser();
   }
 
 }
