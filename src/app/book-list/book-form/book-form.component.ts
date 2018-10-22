615b1f9e7dd4936aceb6878136fe0cb7efd88adb
kwathek <w.kahla16035@pi.tn>
2018-10-22 16:24:19 +0100

    add function add book to component

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder , FormGroup ,Validators } from "@angular/forms";
+import {BooksService} from "../../services/books.service";
+import {Router} from "@angular/router";
+import {Book} from "../../models/book.model";
 
 @Component({
   selector: 'app-book-form',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent implements OnInit {
-
-  constructor() { }
+  bookForm: FormGroup;
+  constructor(private formBuilder: FormBuilder ,
+              private  booksService: BooksService ,
+              private router: Router ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  private initForm() {
+    this.bookForm = this.formBuilder.group({
+        title: ['',Validators.required],
+        author: ['' , Validators.required],
+        synopsis: ''
+    });
+  }
+
+  onSaveBook(){
+      const title = this.bookForm.get('title').value;
+      const author = this.bookForm.get('author').value;
+      const synopsis = this.bookForm.get('synopsis').value;
+      const newBook = new Book(title,author);
+      newBook.synopsis = synopsis ;
+      this.booksService.createNewBook(newBook);
+      this.router.navigate(['/books']);
+  }
 }
